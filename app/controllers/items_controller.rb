82076631ec3295b24183b0524f158b7347cc2c9f
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-11-16 19:00:55 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   private
 
   def item_params

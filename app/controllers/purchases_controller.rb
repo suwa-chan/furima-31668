4fd4e7e2453b5ec89ca0a5a56a35a18284319074
suwa-chan <87.suwa.tetsuya@gmail.com>
2020-11-20 17:07:05 +0900

    商品購入機能の実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,41 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase_address = PurchaseAddress.new
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase != nil
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+     if @purchase_address.valid?
+       pay_item
+       @purchase_address.save
+       redirect_to root_path
+     else
+       render action: :index
+     end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+end
---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  before do
+    @purchase_address = FactoryBot.build(:purchase_address)
+  end
+
+  describe "商品購入機能" do
+    context "商品購入がうまくいく時" do
+      it "token, postal_code, prefecture_id, city, address, phone_numberが存在すれば購入できること" do
+        expect(@purchase_address).to be_valid
+      end
+      it "buildingが存在しなくても購入できること" do
+        @purchase_address.building = nil
+        @purchase_address.valid?
+        expect(@purchase_address).to be_valid
+      end
+    end
+    context "商品購入がうまくいかない時" do
+      it "tokenが空では購入できないこと" do
+        @purchase_address.token = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it "postal_codeが空では購入できないこと" do
+        @purchase_address.postal_code = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it "postal_codeが半角数字でない場合は購入できないこと" do
+        @purchase_address.postal_code = "１２３ー４５６７"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it "postal_codeでハイフンがない場合は購入できないこと" do
+        @purchase_address.postal_code = "1234567"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it "postal_codeのハイフンより前が3桁、後ろが4桁でない場合は購入できないこと" do
+        @purchase_address.postal_code = "1234-567"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code Input correctly")
+      end
+      it "prefecture_idが空の場合は購入できないこと" do
+        @purchase_address.prefecture_id = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Prefecture Select")
+      end
+      it "prefecture_idが1以外で選択されていないと購入できないこと" do
+        @purchase_address.prefecture_id = "1"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Prefecture Select")
+      end
+      it "cityが空では購入できないこと" do
+        @purchase_address.city = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
+      end
+      it "addressが空では購入できないこと" do
+        @purchase_address.address = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it "phone_numberが空では購入できないこと" do
+        @purchase_address.phone_number = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it "phone_numberが半角数字でない場合は購入できないこと" do
+        @purchase_address.phone_number = "１１１１１１１１１１１"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it "phone_numberが11桁以内でない場合は購入できないこと" do
+        @purchase_address.phone_number = "111122223333"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+      it "phone_numberにハイフンが含まれている場合は購入できないこと" do
+        @purchase_address.phone_number = "111-2222-3333"
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number Input only number")
+      end
+    end
+  end
+end
